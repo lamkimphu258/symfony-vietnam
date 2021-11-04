a79f37810cd9ec315d5fc03dbccc2aa95183b9fc
lamkimphu258 <lamkimphu258@gmail.com>
2021-11-04 10:06:48 +0700

    Feature: add signup

--- src/Controller/GeneralController.php
+++ src/Controller/GeneralController.php
@@ -8,19 +8,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GeneralController extends AbstractController
 {
-    #[Route('/', name: 'app-home')]
+    #[Route('/', name: 'app_home')]
     public function home(): Response
     {
         return $this->render('general/home.html.twig');
     }
 
-    #[Route('/about', name: 'app-about')]
+    #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
         return $this->render('general/about.html.twig');
     }
 
-    #[Route('/contributors', name: 'app-contributors')]
+    #[Route('/contributors', name: 'app_contributors')]
     public function contributors(): Response
     {
         return $this->render('general/contributors.html.twig');
